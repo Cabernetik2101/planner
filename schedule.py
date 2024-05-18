04d708aa559af7b8a0e20b6394ab0dd8fde79895
Cabernetik2101 <168580065+Cabernetik2101@users.noreply.github.com>
2024-05-18 10:50:50 +0300

    added exercise on schedule.py

--- schedule.py
+++ schedule.py
@@ -2,3 +2,6 @@ name = int(input("Как твоё имя?: "))
 #access check
 lastname = int(input("Как ваша фамилия?: "))
 print("Доброе пожаловать,", name, lastname)
+exercise = int(input("Сколько упражнений будем делать?: ))
+if exercise < 10:
+  print("мало, надо больше")
