55a17e5db0f4f5a67ccc50ede91674de9181bc0f
Cabernetik2101 <tanya-tihonkova@mail.ru>
2024-05-18 10:46:15 +0300

    access age

--- schedule.py
+++ schedule.py
@@ -1,4 +1,7 @@
 name = int(input("Как твоё имя?: "))
-#access check
 lastname = int(input("Как ваша фамилия?: "))
 print("Доброе пожаловать,", name, lastname)
+
+age = int(input("Сколько вам лет?: "))
+
+
