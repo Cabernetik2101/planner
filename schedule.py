63af2864bbc5b0d7415b0d47db49521ed00bdd61
Cabernetik2101 <tanya-tihonkova@mail.ru>
2024-05-18 10:58:20 +0300

    added control access

--- schedule.py
+++ schedule.py
@@ -3,5 +3,9 @@ lastname = int(input("Как ваша фамилия?: "))
 print("Доброе пожаловать,", name, lastname)
 
 age = int(input("Сколько вам лет?: "))
+if age > 18:
+    print("Доступ открыт.")
+else:
+    print("Доступ закрыт.")
 
 
